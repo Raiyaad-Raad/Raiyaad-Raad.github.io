0c48cb0b590e15c38a77d2058406860a346b0cc6
Raiyaad Raad <77395366+Raiyaad-Raad@users.noreply.github.com>
2024-04-14 13:43:26 +0600

    2024 Commits

--- scripts/sidemenu.js
+++ scripts/sidemenu.js
@@ -1,9 +1,18 @@
-var sidemenu = document.getElementById('sidemenu');
+document.addEventListener('DOMContentLoaded', function() {
+    var sidemenu = document.getElementById('sidemenu');
 
-function openmenu() {
-    sidemenu.style.right = "0";
-}
+    function openmenu() {
+        sidemenu.style.right = "0";
+    }
 
-function closemenu() {
-    sidemenu.style.right = "-200px";
-}
+    function closemenu() {
+        sidemenu.style.right = "-200px";
+    }
+
+    document.querySelector('.fa-bars').addEventListener('click', function() {
+        openmenu();
+    });
+    document.querySelector('.fa-times').addEventListener('click', function() {
+        closemenu();
+    });
+});
